ea95f3f9a82dc92cc0780b09c040f65b9b98c4af
Michael Corey Munn <coreymunn3@gmail.com>
2021-07-30 10:35:19 -0400

    Created and configured redux store, reducer. Implemented in index.

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { unregister } from './core'
+import { unregister, configureStore } from './core'
 import { GlobalStyles } from './styles'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles'
+import { Provider } from 'react-redux'
 
 import { Card, Content, Grid, Title } from './components'
+
+const store = configureStore()
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <Content>
-      <Title>Sodoku</Title>
-      <Card>
-        <Grid />
-      </Card>
-    </Content>
+    <Provider store={store}>
+      <Content>
+        <Title>Sodoku</Title>
+        <Card>
+          <Grid />
+        </Card>
+      </Content>
+    </Provider>
   </ThemeProvider>,
   document.getElementById('root')
 )
