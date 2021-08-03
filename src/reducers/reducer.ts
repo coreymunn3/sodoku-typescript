5ca139bfe0676b0d79ddbb2007d63e158a967c02
Michael Corey Munn <coreymunn3@gmail.com>
2021-08-03 16:14:44 -0400

    Built first create grid action and mapped it to createFullGrid function in the reducer. supporting interfaces, types, and actions files.

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,9 +1,17 @@
 import { AnyAction } from 'redux'
+import { IReducer } from './interfaces'
+import { createFullGrid } from '../utils'
+import * as types from './types'
 
-const initialState = {}
+const initialState: IReducer = {}
 
-function reducer(state = initialState, action: AnyAction) {
+function reducer(state = initialState, action: AnyAction): IReducer {
   switch (action.type) {
+    case types.CREATE_GRID:
+      return {
+        ...state,
+        solvedGrid: createFullGrid(),
+      }
     default:
       return state
   }
